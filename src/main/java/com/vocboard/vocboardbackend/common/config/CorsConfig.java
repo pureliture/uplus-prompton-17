ccaff980ec3d506e0f6b4262bb5558b73a02c65b
pureliture <tkdgur1756@naver.com>
2024-04-16 21:29:09 +0900

    [fix] CorsConfig allowedOrigins 변경

--- src/main/java/com/vocboard/vocboardbackend/common/config/CorsConfig.java
+++ src/main/java/com/vocboard/vocboardbackend/common/config/CorsConfig.java
@@ -9,6 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins("https://turbo-spork-5rjpjrx6pjxh7v9p-3000.app.github.dev/");
     }
 }
